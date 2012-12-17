9e5748530df87a3bb4dc64674a8a3221b610f846
mdy7643 <michael.yeaple@gmail.com>
2012-12-17 00:35:58 -0500

    Everything should be implemented correctly in the Driver class. May have to change the default starting values.

--- src/Driver.java
+++ src/Driver.java
@@ -4,9 +4,49 @@
  * 
  */
 public class Driver {
-
+	
+	// TODO Choose better default values?
+	
+	// Number of resources for the Banker.
+	private final static int nBUnits = 8;
+	// Number of Clients.
+	private final static int nClients = 3;
+	// Number of resources to register for each Client.
+	private final static int nCUnits = 4;
+	// Number of requests for each Client.
+	private final static int nRequests = 2;
+	// Minimum number of milliseconds for each client to sleep.
+	private final static long minSleepMillis = 1000;
+	// Maximum number of milliseconds for each client to sleep.
+	private final static long maxSleepMillis = 3000;
+	
 	public static void main(String[] args) {
+		Client[] clients = new Client[nClients];
 		
+		// Create the banker.
+		Banker banker = new Banker(nBUnits);
+		
+		// Create each of the clients and start them.
+		for (int i = 0; i < nClients; i++) {
+			clients[i] = ( new Client ( "Client " + (i + 1),
+					banker,
+					nCUnits,
+					nRequests,
+					minSleepMillis,
+					maxSleepMillis
+					) );
+			
+			clients[i].start();
+		}
+		
+		// Wait for the clients to finish.
+		for (int j = 0; j < clients.length; j++){
+			try{
+				clients[j].join();
+			} catch (InterruptedException ie) {
+				System.err.println("Error: " + ie.getMessage());
+			}
+		}
 	}
 	
 }
